d9ec085a0ce4a82cc8a0c8d888eb57936154c9e8
Michaud <michaud@venant.nl>
2021-03-29 16:21:00 +0200

    better hilites for the device description

--- js/descriptors/hiliteDescriptors.js
+++ js/descriptors/hiliteDescriptors.js
@@ -1,26 +1,32 @@
-export const hiliteDescriptors = [{
-    element: "device",
-    name: "device_hit",
-    target: "device_overlay"
-},
-{
-    element: "alarm",
-    name: "alarm_hit",
-    target: "alarm_overlay"
-},
-{
-    element: "chronometer",
-    name: "chronometer_hit",
-    target: "chronometer_overlay"
-},
-{
-    element: "clock",
-    name: "clock_hit",
-    target: "clock_overlay"
-},
-{
-    element: "mission_timer",
-    name: "mission_timer_hit",
-    target: "mission_timer_overlay"
-}
+export const hiliteDescriptors = [
+    {
+        element: "device",
+        name: "device_hit",
+        target: "device_overlay"
+    },
+    {
+        element: "mission_timer",
+        name: "mission_timer_hit",
+        target: "mission_timer_overlay"
+    },
+    {
+        element: "chronometer",
+        name: "chronometer_hit",
+        target: "chronometer_overlay"
+    },
+    {
+        element: "clock",
+        name: "clock_hit",
+        target: "clock_overlay"
+    },
+    {
+        element: "alarm",
+        name: "alarm_hit",
+        target: "alarm_overlay"
+    },
+    {
+        element: "device",
+        name: "time_set_hit",
+        target: "time_set_overlay"
+    }
 ];
